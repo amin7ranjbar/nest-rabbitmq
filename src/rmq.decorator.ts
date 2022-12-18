83bda59756b8f93e7c74ba8a2dec6a6f9490d676
a.ranjbar <amin7ranjbar@gmail.com>
2022-12-18 09:04:22 +0330

    fix: use only queue required

--- src/rmq.decorator.ts
+++ src/rmq.decorator.ts
@@ -6,16 +6,25 @@ import {
 } from '@golevelup/nestjs-rabbitmq';
 import { DEFAULT_EXCHANGE_NAME } from './rmq.constant';
 
-export const Rpc = (options: MessageHandlerOptions) =>
+type MessageOptions = Pick<MessageHandlerOptions, 'queue'> & {
+  routingKey?: string | string[];
+  exchange?: string;
+  timeout?: number;
+  headers?: any;
+};
+
+export const Rpc = (options: MessageOptions) =>
   RabbitRPC({
     ...options,
+    routingKey: options.routingKey || options.queue,
     exchange: options.exchange || DEFAULT_EXCHANGE_NAME,
     errorBehavior: MessageHandlerErrorBehavior.NACK,
   });
 
-export const Subscribe = (options: MessageHandlerOptions) =>
+export const Subscribe = (options: MessageOptions) =>
   RabbitSubscribe({
     ...options,
+    routingKey: options.routingKey || options.queue,
     exchange: options.exchange || DEFAULT_EXCHANGE_NAME,
     errorBehavior: MessageHandlerErrorBehavior.NACK,
   });
